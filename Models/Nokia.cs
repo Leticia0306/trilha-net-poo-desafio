aff50ac3325c7c0876bcc50f29e7217d40914e5a
Leticia Vieira <leticiav.rosa@outlook.com>
2024-01-13 17:41:26 -0200

    Desafio concluido!

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    //IMPLEMENTADO
+    //Foi herdado a classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        //IMPLEMENTADO
+        //Foi criado o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//IMPLEMENTADO
+class Program
+{
+    static void Main()
+    {
+        //IMPLEMENTADO
+        //Foi criado instâncias de Nokia
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia(numero: "(11)93344-5009", modelo: "Nokia XR20", imei: "102030405", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("\n");
+        
+        //IMPLEMENTADO
+        //Foi criado instâncias do Iphone
+        Console.WriteLine("Smartphone Iphone:");
+        Smartphone iphone = new Iphone(numero: "(11)96698-8544", modelo: "Iphone 15", imei: "152535455", memoria: 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Youtube");
+
+        //Aguarda a entrada do usuário antes de encerrar o programa
+        Console.ReadLine();
+        
+    }
+}
